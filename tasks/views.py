07dc38315294b6f54a542f56f3baa670f8285a6b
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-18 13:31:28 -0500

    List completed tasks, route protection

--- tasks/views.py
+++ tasks/views.py
@@ -7,13 +7,13 @@ from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
-
 
 def signup(request):
 
@@ -42,10 +42,17 @@ def signup(request):
             'error': 'Passwords must match'
         })
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {'tasks': tasks})
 
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+@login_required
 def create_task(request):
     
     if request.method == 'GET':
@@ -65,6 +72,7 @@ def create_task(request):
                 'error': 'Bad data passed in. Try again.'
             })
 
+@login_required
 def task_detail(request, task_id):
     if request.method == 'GET':
         task = get_object_or_404(Task, pk=task_id, user=request.user)
@@ -83,19 +91,22 @@ def task_detail(request, task_id):
                 'error': 'Bad info passed in. Try again.'
             })
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.datecompleted = timezone.now()
         task.save()
         return redirect('tasks')
-    
+
+@login_required   
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.delete()
         return redirect('tasks')
 
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
